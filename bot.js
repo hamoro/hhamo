4eb84d50d9a6af81723e9d379107cf44b5be75eb
hamoro <40411863+hamoro@users.noreply.github.com>
2018-06-21 17:49:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,15 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
-      }
-});
+const bot = new Discord.Client();
 
 
 
-// THIS  MUST  BE  THIS  WAY
+
+
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
