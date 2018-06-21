687581216010c2f4ac3f1f7d58c2229e2f1378e3
hamoro <40411863+hamoro@users.noreply.github.com>
2018-06-21 17:47:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,78 +11,45 @@ client.on('message', message => {
       }
 });
 
-client.on('message',function(message) {
-    let muteRole = message.guild.roles.find(r => r.name === "Muted");
-    let muteId = message.mentions.users.first();
+client.on('message', async message => {
+    let muteReason = message.content.split(" ").slice(3).join(" ");
+    let mutePerson = message.mentions.users.first();
     let messageArray = message.content.split(" ");
-    let muteReason = messageArray[3];
-    let Swearing = '1h';
-    let Advertising = '4h';
-    let Spam = '2h';
-   if(message.content.startsWith(prefix + "mute")) {
-       if(!muteRole) return message.guild.createRole({ name: "Muted", permissions: [] });
-       if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return message.channel.send("**- You don't have the needed permissions!**");
-       if(!muteId) return message.channel.send("**- Mention someone!**");
-       if(muteId === message.author) return message.channel.send('**- You cannot mute yourself!**');
-       if(muteId === client.user) return message.channel.send('**- You cannot mute me!**');
-       message.guild.channels.forEach((channel, id) => {
+    let muteRole = message.guild.roles.find(r => r.name === 'Muted');
+    let time = messageArray[2];
+    if(message.content.startsWith(prefix + "tempmute")) {
+        if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return message.channel.send('**- You don\'t have the needed permissions!**');
+        if(!mutePerson) return message.channel.send("**- Mention someone!**");
+        if(mutePerson === message.author) return message.channel.send('**- You cannot mute yourself!**');
+        if(mutePerson === client.user) return message.channel.send('**- You cannot mute me!**');
+        if(message.guild.member(mutePerson).roles.has(muteRole.id)) return message.channel.send('**- This person is already muted!**');
+        if(!muteRole) return message.guild.createRole({ name: "Muted", permissions: [] });
+        if(!time) return message.channel.send("**- Supply a time!**");
+        if(!time.match(/[1-7][s,m,h,d,w]/g)) return message.channel.send('**- Supply a real time!**');
+        if(!muteReason) return message.channel.send("**- Supply a reason!**");
+        message.guild.channels.forEach(async (channel, id) => {
       message.channel.overwritePermissions(muteRole, {
         SEND_MESSAGES: false,
         ADD_REACTIONS: false
       });
     });
-    message.channel.send(`
-    \`\`\`ml
-" قم بأختيار رقم السبب "
-1 : السب و الشتم
-2 : النشر
-3 : السبام
-\`\`\`
-__امامك 20 ثانية للاختيار__`)
-.then(() => {
-  message.channel.awaitMessages(response => response.content === '1', {
-    max: 1,
-    time: 20000,
-    errors: ['time'],
-  })
-  .then((collected) => {
-      message.guild.member(muteId).addRole(muteRole)
-      .then(() => { setTimeout(() => {
-           message.guild.member(muteId).removeRole(muteRole);
-       }, mmss(Swearing));
-       message.channel.send(`**تم!, تم اعطاء ميوت لـ${muteId} بسبب السب و الشم**`);
-      });
+        message.guild.member(mutePerson).addRole(muteRole);
+        let muteEmbed = new Discord.RichEmbed()
+        .setAuthor(`${mutePerson.username}#${mutePerson.discriminator}`,mutePerson.avatarURL)
+        .setTitle(`You have been muted in ${message.guild.name}`)
+        .setThumbnail(message.guild.iconURL)
+        .addField('- Muted By:',message.author,true)
+        .addField('- Reason:',muteReason,true)
+        .addField('- Duration:',`${mmss(mmss(time), {long: true})}`)
+        .setFooter(message.author.username,message.author.avatarURL);
+        message.guild.member(mutePerson).sendMessage(muteEmbed)
+        message.channel.send(`**- Done!, I muted: ${mutePerson}**`)
+        .then(() => { setTimeout(() => {
+           message.guild.member(mutePerson).removeRole(muteRole);
+       }, mmss(time));
     });
-
-message.channel.awaitMessages(response => response.content === '2', {
-    max: 1,
-    time: 20000,
-    errors: ['time'],
-  })
-  .then((collected) => {
-      message.guild.member(muteId).addRole(muteRole)
-      .then(() => { setTimeout(() => {
-           message.guild.member(muteId).removeRole(muteRole);
-       }, mmss(Advertising));
-       message.channel.send(`**تم اعطاء ميوت لـ${muteId} بسبب النشر**`);
-      });
-    });
-message.channel.awaitMessages(response => response.content === '3', {
-    max: 1,
-    time: 20000,
-    errors: ['time'],
-  })
-  .then((collected) => {
-      message.guild.member(muteId).addRole(muteRole)
-      .then(() => { setTimeout(() => {
-           message.guild.member(muteId).removeRole(muteRole);
-       }, mmss(Spam));
-       message.channel.send(`**تم اعطاء ميوت لـ${muteId} بسبب السبام**`);
-      });
-    });
-   });
-   }
-});
+    }
+})
 
 
 // THIS  MUST  BE  THIS  WAY
